8dc770f4a0a5ffdfb7cc95eea7da39cfe674d243
oava <omarvega598@gmail.com>
2024-01-23 15:50:11 -0500

    Feat(Footer): Add global footer componen t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component,} from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import {Event, NavigationEnd, Router} from "@angular/router";
 
 register();
 @Component({
@@ -8,5 +9,16 @@ register();
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  currentRoute : string;
+  constructor(private router : Router) {
+    this.currentRoute = "";
+    this.router.events.subscribe((event : Event) => {
+      if (event instanceof  NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    })
+  }
+  showFooter() {
+    return this.currentRoute !== "/intro"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {FooterComponentComponent} from "./component/footer-component/footer-component.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponentComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
